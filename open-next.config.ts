42939971ea1377d36c3835fdbcf508a6db52cbf0
MayerZhe <myonging@gmail.com>
2026-06-01 02:44:46 +0800

    sync: 2026-06-01 from monorepo

---
+++ open-next.config.ts
@@ -0,0 +1,4 @@
+// @ts-nocheck — @opennextjs/cloudflare is only available in Cloudflare Pages build env
+import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
+
+export default defineCloudflareConfig({});
